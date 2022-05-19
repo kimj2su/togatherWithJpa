9a000808e824294aabbed0448b55297ac0674068
Jisu Kim <95600042+jisu3316@users.noreply.github.com>
2022-05-19 23:39:31 +0900

    Update TogatherWithJpaApplication.java

--- src/main/java/team1/togather/TogatherWithJpaApplication.java
+++ src/main/java/team1/togather/TogatherWithJpaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TogatherWithJpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TogatherWithJpaApplication.class, args);
+		SpringApplication.run(TogatherWithJpaApplication.class, args); 
 	}
 
 }
