23902d3f4b553382872d96aa0e393466ba4f85ef
jisu3316 <jisu3316@naver.com>
2022-08-16 23:33:49 +0900

    뷰 파일 추가

--- src/main/resources/static/js/createMember.js
+++ src/main/resources/static/js/createMember.js
@@ -1,30 +1,30 @@
 $(function(){
-    $('#already-nickname').on("click" , function(){
-        if($('#nickname').val().length == 0 || $('#nickname').val() == null ) return alert("닉네임을 입력하세요.");
+    $('#already-userId').on("click" , function(){
+        if($('#userId').val().length == 0 || $('#userId').val() == null ) return alert("아이디를 입력하세요.");
         $.ajax({
             type:'GET',
-            url:'/members/nicknameCheck',
-            data:{nickname:$('#nickname').val()},
+            url:'/members/userIdCheck',
+            data:{userId:$('#userId').val()},
             success: function(data) {
                 // $('#nicknameMessage').text(""+data+"");
                 // $('#nicknameMessage').css('font-weight' ,  "bold");
                 console.log(data);
                 if(data == 1){
-                    $('#nicknameMessage').text("이미 사용중인 닉네임 입니다.");
+                    $('#userIdMessage').text("이미 사용중인 아이디 입니다.");
                     $('#sign-up-button').removeClass("btn btn-primary disabled");
                     $('#sign-up-button').addClass("btn btn-primary disabled");
                 } else if(data == 0){
-                    $('#nicknameMessage').text("사용 가능한 닉네임 입니다.");
+                    $('#userIdMessage').text("사용 가능한 아이디 입니다.");
                     $('#sign-up-button').removeClass("btn btn-primary disabled");
                     $('#sign-up-button').addClass("btn btn-primary");
                 } else if(data == 3){
-                    $('#nicknameMessage').text("사용 불가능한 닉네임 입니다.");
+                    $('#userIdMessage').text("사용 불가능한 닉네임 입니다.");
                 } else {
-                    $('#nicknameMessage').text("사용 가능한 닉네임 입니다.");
+                    $('#userIdMessage').text("사용 가능한 닉네임 입니다.");
                 }
             },
             error:function(data){
-                $('#messages').text(data);
+                $('#userIdMessage').text(data);
             }
         });
     });
